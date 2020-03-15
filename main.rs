e11450a5b29433785db91ee32d3a2852d71706b5
poojakhanal <khanalpooja425@gmail.com>
2020-03-15 14:07:19 -0500

    working code

--- main.rs
+++ main.rs
@@ -4,37 +4,53 @@ use std::fs::File;
 use std::io::BufRead;
 use std::path::Path;
 
+mod player;
+use player::{get_player, Player};
+
+mod report;
+use report::{print_reports, handle_print_to_file};
+
 fn main(){
-    println!("Input the file name");
+    println!("Input the file name"); 
+    match get_all_players() {
+        Err(error) => println!("{}", error),
+        Ok(results) => {
+            print_reports(&results, None);
+            handle_print_to_file(&results);
+        }
+    }
+}
+
+fn get_all_players() -> Result<Vec<Result<Player,String>>, String> {
     let mut fname = String::new();
-    
+    let mut results: Vec<Result<Player, String>> = Vec::new();
     match io::stdin().read_line(&mut fname){
         Ok(_) => {
-            println!("{}",&fname);
             fname = fname.trim_end().to_string();
-            // fname = fname.to_string();
-            if let Ok(lines) = read_lines(fname) {
-                // Consumes the iterator, returns an (Optional) String
-                for line in lines {
-                    if let Ok(ip) = line {
-                        println!("{}", ip);
+            match read_lines(&fname) {
+                Ok(lines) => {
+                    // Consumes the iterator, returns an (Optional) String
+                    let mut line_count: i32 = 0;
+                    for line_res in lines {
+                        line_count += 1;
+                        if let Ok(line) = line_res {
+                            let result = get_player(&line.split(" ").collect::<Vec<&str>>(), line_count);
+                            results.push(result);
+                        }
                     }
-                }
+                    Ok(results)
+                },
+                Err(_) => Err(format!("Could not read file {}", &fname))
             }
         },
         Err(_) => {
-            println!("Error reading the filename")
-        }
-        
+            Err("Error reading the file name".to_string())
+        }  
     }
+}
 
-
-
-
-    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     where P: AsRef<Path>, {
         let file = File::open(filename)?;
         Ok(io::BufReader::new(file).lines())
-    }
-
 }
--- player.rs
+++ player.rs
@@ -1,51 +1,61 @@
 //Player Structure 
 
-struct Player {
-   firstName, lastName:string,
-   plateAppearances, atBats, singles, doubles, triples, homeRuns, walks, hitByPitch:int
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+pub struct Player {
+    last_name: String,
+    first_name: String,
+    plate_appearances: i32,
+    at_bats: i32,
+    singles: i32,
+    doubles: i32,
+    triples: i32,
+    home_runs: i32,
+    walks: i32,
+    hit_by_pitch: i32,
 }
 
-enum Result<T, E> {
-   Ok(T),
-   Err(E),
+impl Player {
+    // function to get the batting average of a player
+    pub fn get_batting_average(&self) -> f32{
+        let p = self;
+        (p.singles as f32+p.doubles as f32+p.triples as f32+p.home_runs as f32) / p.at_bats as f32
+    }
+
+    //function to get he slugging of a player
+    pub fn get_slugging_percentage(&self) -> f32 {
+        let p = self;
+        (p.singles as f32+2.0*p.doubles as f32+3.0*p.triples as f32+4.0*p.home_runs as f32) / p.at_bats as f32
+    }
+
+    //function to get Onbase percentage
+    pub fn get_on_base_percentage(&self) -> f32 {
+        let p = self;
+        (p.singles as f32+p.doubles as f32+p.triples as f32+p.hit_by_pitch as f32+p.walks as f32) / p.plate_appearances as f32
+    }
+
+    pub fn get_name(&self) -> String {
+        self.last_name.clone() + ", " + &self.first_name
+    }
 }
 
-// function to get the batting average of a player
-fn getBattingAverage(p Player) -> f64{
-    (p.singles+p.doubles+p.triples+p.homeRuns) / p.atBats 
-}
 
-//function to get he slugging of a player
-fn getSluggingPercentage(p Player) -> f64 {
-	(p.singles+2*p.doubles+3*p.triples+4*p.homeRuns) / p.atBats 
-}
-
-//function to get Onbase percentage
-fn getOnBasePercentage(p Player) -> f64 {
-	(p.singles+p.doubles+p.triples+p.hitByPitch+p.walks) / p.plateAppearances 
-}
-
-// ByLastName Comparator to sort players by last name
-// type ByLastName []Player
-
-fn getPlayer(s []string, lineCount int) -> Player{
-	err := checkErrors(s, lineCount)
+pub fn get_player(s: &Vec<&str>, line_count: i32) -> Result<Player, String>{
+	let err = check_errors(s, line_count);
     match err {
-        Ok(ok) => {
-            let player =  Player;
-            player = Player{
-                firstName = s[0],
-                lastName=   s[1],
-            }
-            player.plateAppearances = s[2].parse::<f64>().unwrap();
-            player.atBats= s[3].parse::<f64>().unwrap();
-            player.singles= s[4].parse::<f64>().unwrap();
-            player.doubles = s[5].parse::<f64>().unwrap();
-            player.triples= s[6].parse::<f64>().unwrap();
-            player.homeRuns= s[7].parse::<f64>().unwrap();
-            player.walks= s[8].parse::<f64>().unwrap();
-            player.hitByPitch= s[9].parse::<f64>().unwrap();
-            player
+        Ok(_) => {
+            let player = Player{
+                first_name: s[0].to_string(),
+                last_name:   s[1].to_string(),
+                plate_appearances: s[2].parse::<i32>().unwrap(),
+                at_bats: s[3].parse::<i32>().unwrap(),
+                singles: s[4].parse::<i32>().unwrap(),
+                doubles: s[5].parse::<i32>().unwrap(),
+                triples: s[6].parse::<i32>().unwrap(),
+                home_runs: s[7].parse::<i32>().unwrap(),
+                walks: s[8].parse::<i32>().unwrap(),
+                hit_by_pitch: s[9].parse::<i32>().unwrap(),
+            };
+            Ok(player)
         }
         Err(e) => Err(e)
     }
@@ -53,18 +63,18 @@ fn getPlayer(s []string, lineCount int) -> Player{
 }
 
 //check for errors in the line
-fn checkErrors(s []string, lineCount int) -> Result <(), Err> {
+fn check_errors(s: &Vec<&str>, line_count: i32) -> Result <(), String> {
 	if s.len() < 10 {
-		Err("line {} Line contains not enough data", lineCount)
+		return Err(format!("Line ({}) : line contains not enough data", line_count));
 	}
-    
-	for i := 2; i < 10; i++ {
+
+	for i in 2..10 {
         let test = s[i].parse::<f64>();
         match test {
-            Err(_) => return Err("Line ({}) : line contains invalid numeric data", linecount),
+            Err(_) => return Err(format!("Line ({}) : line contains invalid numeric data", line_count)),
             Ok(_) => () // do nothing
         }
 	}
-	Result
+    Ok(())
 }
 
